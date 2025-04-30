2f62ae52662c16f0a937fe4ca0eb09bdc3d1f675
FogozIV <fogoziv@gmail.com>
2025-04-30 21:44:39 +0200

    Added cast

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,13 @@ void setup() {
   Wire.beginTransmission(0x22);
   Wire.write(0x00);
   uint16_t data = 500;
-  Wire.write(&data, 2);
+  Wire.write((uint8_t*)&data, 2);
   Wire.endTransmission();
   delay(2000);
   Wire.beginTransmission(0x22);
   Wire.write(0x00);
   data = 16000;
-  Wire.write(&data, 2);
+  Wire.write((uint8_t*)&data, 2);
 
 // write your initialization code here
 }
